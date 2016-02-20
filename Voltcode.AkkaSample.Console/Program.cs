f96922d380b8b36df8598306b332beac05c82f4e
voltcode <konrad.volt@gmail.com>
2016-02-20 22:58:56 +0100

    added console project for easier bug replication

---
+++ Voltcode.AkkaSample.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Voltcode.AkkaSample.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var actorSystem = ActorSystem.Create("voltcode");
+
+            System.Console.ReadKey();
+        }
+    }
+}
